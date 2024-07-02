bbf438a83ce0c21f004834756b9be84306856e6e
trevor james nangosha <trevornangosha16@gmail.com>
2024-07-02 22:16:54 +0300

    Deployed f9e22be with MkDocs version: 1.6.0

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,7 +1,7 @@
-// app.location$.subscribe(function (url) {
-//     window.dataLayer = window.dataLayer || [];
-//     function gtag(){dataLayer.push(arguments);}
-//     gtag('js', new Date());
+app.location$.subscribe(function (url) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-//     gtag('config', 'G-GNG4PJ8C5Z');
-// })
+    gtag('config', 'G-GNG4PJ8C5Z');
+})
